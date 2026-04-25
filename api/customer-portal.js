fa325af3fa745ea1326d2070424c99a758871102
faitacrea <faitacreapro@gmail.com>
2026-04-25 11:58:23 +0200

    Update customer-portal.js

--- api/customer-portal.js
+++ api/customer-portal.js
@@ -1,8 +1,8 @@
-const Stripe = require('stripe');
+import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
