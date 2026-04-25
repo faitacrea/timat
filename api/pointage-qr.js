60e1cf9657fd19063d18054824eb7ce657a77316
faitacrea <faitacreapro@gmail.com>
2026-04-25 11:58:02 +0200

    Update pointage-qr.js

--- api/pointage-qr.js
+++ api/pointage-qr.js
@@ -1,11 +1,11 @@
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
 );
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   const { enfant, date, type, action } = req.query;
 
   // If POST: process the validation
